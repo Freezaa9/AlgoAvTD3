b6371355bccfa47a8a014ff58880a551f8273303
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-05-18 14:28:14 +0200

    init architecture

---
+++ AlgoAvTd3/src/algoavtd3/NavigableMapList.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoavtd3;
+
+/**
+ *
+ * @author Antoine
+ */
+public class NavigableMapList implements Dico {
+    
+    
+    private class ABR<K, V> {
+        
+        private class Noeud<K, V> {
+            K cle;
+            V valeur;
+            ABR<K, V> fg; // ne doit jamais être null (et pas testé == null) !!
+                           // L'arbre peut etre vide mais jamais null
+            ABR<K, V> fd; // ne doit jamais être null (et pas testé == null) !!
+                            // Ne pas confondre vide et n'existe pas
+            
+        }
+    }
+    
+    
+}
