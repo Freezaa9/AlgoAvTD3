b9c774d70c57aa404f336d1ffcb1537a032f3872
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-05-18 15:34:32 +0200

    ABR.ajouter(K clé,V valeur)

--- AlgoAvTd3/src/algoavtd3/ABR.java
+++ AlgoAvTd3/src/algoavtd3/ABR.java
@@ -5,9 +5,6 @@
  */
 package algoavtd3;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 /**
  *
  * @author Antoine
@@ -24,14 +21,27 @@ public class ABR<K extends Comparable, V > implements Dico<K, V>{
     }
 
     /**
-     *
-     * @param cle
-     * @param valeur
-     * @return
+     * Ajouter un élément une combinaison clé, valeur dans le Dico.
+     * @param pCle La clé a laquelle la valeur doit être inserer.
+     * @param pValeur la valeur à inserer.
+     * @return La valeur remplacé si elle existe, null sinon
      */
     @Override
-    public V ajouter(K cle, V valeur) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public V ajouter(K pCle, V pValeur) {
+        if(estVide()){
+            racine = new Noeud(pCle, pValeur);
+            return null;
+        }
+        if(pCle.compareTo(racine.cle) == 0 ){
+            V tmpValeur = (V) racine.valeur;
+            racine.valeur = pValeur;
+            return tmpValeur;
+        }
+        if(pCle.compareTo(racine.cle) <= 0 ){
+            return (V) racine.fg.ajouter(pCle, pValeur);
+        }
+        
+        return (V) racine.fd.ajouter(pCle, pValeur);
     }
     
 
@@ -81,6 +91,10 @@ public class ABR<K extends Comparable, V > implements Dico<K, V>{
         return racine.cle != null;
     }
 
+    private boolean estVide() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     private class Noeud<K extends Comparable, V> {
         K cle;
         V valeur;
@@ -100,6 +114,6 @@ public class ABR<K extends Comparable, V > implements Dico<K, V>{
             fg = new ABR();
             fd = new ABR();
         }
-
+        
     }
 }
